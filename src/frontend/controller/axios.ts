2ae4ccdfe693abf3bcecd108611d17059aad1fe6
root <root@peter.peter>
2023-04-07 02:26:50 +0530

    base url added axios

--- src/frontend/controller/axios.ts
+++ src/frontend/controller/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const API  = axios.create({
-baseURL: process.env.baseURL || "http://localhost:8080",
+baseURL: process.env.baseURL as string || "http://localhost:8080",
 headers: {
     "content-type": "application/json",
     "accept": "application/josn",
