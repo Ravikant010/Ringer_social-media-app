a8b5c8381a3f20ececed83497070c2b7b4c31180
root <root@peter.peter>
2023-04-07 04:00:40 +0530

    add server link axios

--- src/backend/server/server.ts
+++ src/backend/server/server.ts
@@ -6,7 +6,7 @@ import cors from "cors"
 const app = express()
 dotenv.config({path: "/root/Downloads/ringer/.env"})
 console.log(process.env.PORT)
-app.use(cors({origin:["http://localhost:8081", "http://192.168.1.5:8081", "http://192.168.43.221:8081", "https://ringer-social-media-app.vercel.app"], credentials: true}))
+app.use(cors({origin:["*"], credentials: true}))
 app.use(express.json())
 app.use(Route)
 connectDB();
