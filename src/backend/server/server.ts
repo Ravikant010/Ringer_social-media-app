c0fa75ca802c085e862503607df3df781ee1c5b3
root <root@peter.peter>
2023-04-07 09:16:31 +0530

    fixing cors

--- src/backend/server/server.ts
+++ src/backend/server/server.ts
@@ -6,7 +6,22 @@ import cors from "cors"
 const app = express()
 dotenv.config({path: "/root/Downloads/ringer/.env"})
 console.log(process.env.PORT)
-app.use(cors({origin:["http://localhost:8081","https://ringer-social-media-app.vercel.app","http://ringer-social-media-app.vercel.app", "https://ringer.vercel.app", "http://ringer.vercel.app"], credentials: true}))
+const corsOptions = {
+    origin: function (origin:any, callback:any) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true
+  }
+  
+  app.use(cors(corsOptions));
+  
+const allowedOrigins = ["http://localhost:8081","https://ringer-social-media-app.vercel.app","http://ringer-social-media-app.vercel.app", "https://ringer.vercel.app", "http://ringer.vercel.app"];
+
+
 app.use(express.json())
 app.use(Route)
 connectDB();
